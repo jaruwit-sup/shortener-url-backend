72b6ca4d2a02d3631cf23aab28dbdb15b2eeda61
Jaruwit Supsuantaeng <jaruwits@wisesoft.co.th>
2023-01-05 16:10:21 +0700

    refactor: add example request body

--- src/urls/dto/create-short-url.dto.ts
+++ src/urls/dto/create-short-url.dto.ts
@@ -1,5 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsUrl } from 'class-validator';
 export class CreateShortUrlDto {
+  @ApiProperty({ example: 'https://google.com' })
   @IsUrl(undefined, { message: 'URL is not valid.' })
   longUrl: string;
 }
