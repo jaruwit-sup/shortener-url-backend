b59cbe672c220f39edc943ad06d0d0b6c025e300
Jaruwit Supsuantaeng <jaruwits@wisesoft.co.th>
2023-01-07 12:18:57 +0700

    refactor: edit for support cors

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,21 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('Shortener URLs API')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  // Cors
+  app.enableCors();
+
   await app.listen(configService.getPort());
 }
 bootstrap();
